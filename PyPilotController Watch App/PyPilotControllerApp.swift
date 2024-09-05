d55479725de32f375c6448f44f3d37c9b4156427
Francisco Gorina Vanrell <fgorina@gmail.com>
2024-09-05 16:20:02 +0200

    Initial Commit

---
+++ PyPilotController Watch App/PyPilotControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyPilotControllerApp.swift
+//  PyPilotController Watch App
+//
+//  Created by Francisco Gorina Vanrell on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PyPilotController_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
