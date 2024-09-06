e80f8bed6d4e811dadbce5a7c8fbb3ddf2afa56c
Francisco Gorina Vanrell <fgorina@gmail.com>
2024-09-06 10:24:24 +0200

    Before changing to compass view

--- PyPilotController Watch App/ContentView.swift
+++ PyPilotController Watch App/ContentView.swift
@@ -8,15 +8,110 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @Environment(\.scenePhase) var phase : ScenePhase
+    @ObservedObject var server = PyPilot.shared
+
+    @State var targetRhumb : Double = 0
+    @State var currentRhumb : Double = 360
+    @State var mode = 0.0
+    @State var editable = false
+    
+    @FocusState private var targetFocusState : Bool
+    @FocusState private var modeFocusState : Bool
+    
+    let modeNames = ["Rudder", "Compass", "GPS", "Wind", "True Wind"]
+        var body: some View {
+        
+            VStack{
+                Text("\(Int(targetRhumb.rounded()))")
+                    .font(.title)
+                    .focusable(editable)
+                    .focused($targetFocusState)
+                    .padding([.leading, .trailing], 10)
+                    .border(targetFocusState ? .green : .clear)
+                    .digitalCrownRotation($targetRhumb, from: 0.0, through: 359.0, by: 1.0, sensitivity: .medium, isContinuous: true, isHapticFeedbackEnabled: true)
+                    .onTapGesture {
+                        if editable {
+                            if targetFocusState {
+                                server.setTarget(targetRhumb)
+                            }
+                            mode = Double(server.mode)
+                            
+                            editable = false
+                        }else{
+                            editable = true
+                            targetFocusState = true
+                        }
+                    }
+                    
+                Spacer()
+                HStack{
+                    Image(systemName: "arrow.turn.up.left")
+                        .frame(width: 50, height: 60)
+                        .onLongPressGesture {
+                            WKInterfaceDevice.current().play(.success)
+                            server.tackPort()
+                     }
+                    Spacer()
+                    Text("\(Int(server.heading))")
+                        .font(.largeTitle)
+                        
+                        .onTapGesture {
+                            if editable {
+                                editable = false
+                                targetRhumb = Double(server.target)
+                                mode = Double(server.mode)
+                                WKInterfaceDevice.current().play(.failure)
+                            }
+                        }
+                    Spacer()
+                    Image(systemName: "arrow.turn.up.right")
+                        .frame(width: 50, height: 60)
+                        .onLongPressGesture {
+                            WKInterfaceDevice.current().play(.success)
+                            server.tackStarboard()
+                    }
+                }
+                Spacer()
+                     
+                Text("\(modeNames[Int(mode)])").font(.title)
+                    .focusable(editable)
+                    .padding([.leading, .trailing], 10)
+                    .focused($modeFocusState)
+                    .border(modeFocusState ? .green : .clear)
+                    .digitalCrownRotation($mode, from: 0.0, through: 4.0, by: 1.0, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: true)
+                    .onTapGesture {
+                        if editable{
+                            if modeFocusState {
+                                server.setMode(Int(mode))
+                            }
+                            targetRhumb = Double(server.target)
+                            
+                            editable = false
+                        }else{
+                            editable = true
+                            modeFocusState = true
+                        }
+                    }
+                    
+            } .onChange(of: phase) { oldValue, newValue in
+                switch newValue {
+                case .active:
+                    server.connect()
+                    targetRhumb = server.target
+                    currentRhumb = server.heading
+                    mode = Double(server.mode)
+                    
+                    break;
+                case .inactive:
+                    server.close()
+                    break;
+                default:
+                    break
+                }
+            }
+
         }
-        .padding()
-    }
 }
 
 #Preview {
--- PyPilotController Watch App/PyPilot.swift
+++ PyPilotController Watch App/PyPilot.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+class PyPilot : ObservableObject{
+    
+    enum TackState : Int {
+        case none = 0
+        case begin = 1
+        case waiting = 2
+        case tacking = 3
+    }
+    
+    enum TackDirection : Int {
+        case port = -1
+        case none = 0
+        case starboard = 1
+    }
+    static var shared : PyPilot = PyPilot()
+    
+    var heading : Double = 180.0
+    var target : Double = 90.0
+    var mode : Int = 0
+    var rudderAngle : Double = 0.0
+    var tackState : TackState = .none
+    var tackDirection : TackDirection = .none
+    
+    
+    init() {
+        print("Creating a PyPilot object")
+    }
+    
+    func connect(){
+         print("Connecting to PyPilot")
+        
+        
+    }
+    
+    func close(){
+        print("Disconnecting to PyPilot")
+    }
+    
+    func setTarget(_ rhumb : Double){
+        target = rhumb
+        print("Sending \(rhumb) to PyPilot")
+    }
+    
+    func setMode(_ mode : Int){
+        self.mode = mode
+        print("Setting mode to  \(mode) to PyPilot")
+    }
+    
+    func tackPort(){
+        print("Sending tack port to PyPilot")
+    }
+    
+    func tackStarboard(){
+        print("Sending tack Starboard to PyPilot")
+    }
+}
